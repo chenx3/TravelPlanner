0fbf3b554a721bf5d9aaf84fd9dd451bf0cade4d
Xi Chen <chenx2@carleton.edu>
2016-12-08 15:11:24 +0100

    added interface

---
+++ app/src/main/java/com/example/chenx2/travelplanner/OnResultInterface.java
@@ -0,0 +1,10 @@
+package com.example.chenx2.travelplanner;
+
+import android.content.Intent;
+
+/**
+ * Created by chenx2 on 12/5/2016.
+ */
+public interface OnResultInterface {
+    void onActivityResult(int requestCode, int resultCode, Intent data);
+}
